64b9313dba261bf6104f7b4953a9d283b353815f
Keren Nzira <kerennzira@gmail.com>
2026-01-09 12:20:24 +0100

    refactor: improve code formatting and structure for clarity

--- C program/sum_of_positive_numbers.c
+++ C program/sum_of_positive_numbers.c
@@ -1,18 +1,24 @@
-//TD3:EX5 Écrire un programme qui affiche les diviseurs d’un entier positif n non nul.
+// TD3:EX5 Écrire un programme qui affiche les diviseurs d’un entier positif n non nul.
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main()
+{
     int n, div;
+    do
+    {
+        printf("Enter a number.\n");
+        scanf("%d", &n);
+    } while (n <= 0);
 
-    printf("Enter a number.\n");
-    scanf("%d",&n);
-
-    for(int i=1;i<=n;i++){
-       if(n%i==0){
-        printf("%d \t",i);
-       }
+    for (int i = 1; i <= n; i++)
+    {
+        div = n % i;
+        if (div == 0)
+        {
+            printf("%d \t", i);
+        }
     }
     return 0;
 }
