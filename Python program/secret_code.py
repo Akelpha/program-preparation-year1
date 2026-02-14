11a325dd0ebaf5e63f4ae14c769dcc0c20052a50
Keren Nzira <kerennzira@gmail.com>
2026-02-14 17:19:33 +0100

    feat: enhance guessing game logic with improved feedback and structure

--- Python program/secret_code.py
+++ Python program/secret_code.py
@@ -5,15 +5,44 @@
 # dire si le nombre est trop grand ou trop petit
 # compter le nombre de tentatives
 # afficher un message spécial selon la performance
+# plus petit → afficher “Trop petit”
 
-Ce que tu vas utiliser
+# plus grand → afficher “Trop grand”
 
-while
+# égal → afficher “Bravo” + sortir de la boucle
+# Ce que tu vas utiliser
 
-if / elif / else
+# while
 
-variables
+# if / elif / else
 
-compteur
+# variables
 
-comparaison
+# compteur
+
+# comparaison
+# TODO : utiliser la bibliothèque random pour générer le nombre secret aléatoire quand je comprendrai mieux
+
+secret_number= 94;
+tentative =0;
+while True:
+    user_number = int(input("Can you guess the number: "))
+    tentative +=1
+    if user_number <secret_number:
+        print("Too small")
+        
+    elif user_number>secret_number:
+        print("Too big")
+
+    else:
+        print("You guess the number!!")
+        break
+    
+if tentative <=5:
+    print("You guess this number quickly")
+elif 6 <= tentative <= 10:
+    print("You could guess it faster😔!")
+else :
+    print("You could have done better!")
+
+
