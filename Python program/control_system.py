c97365cfb251819d14934ce709dd8aa68d378918
Keren Nzira <kerennzira@gmail.com>
2026-02-17 19:41:42 +0000

    feat: add control system and notes manager exercises with detailed instructions

---
+++ Python program/control_system.py
@@ -0,0 +1,13 @@
+"""🧠 Exercice : Mini système de contrôle d’accès
+Tu dois écrire un programme qui :
+Demande à l’utilisateur :
+son nom
+son âge
+le nombre de tentatives qu’il a déjà faites
+Le programme doit :
+Refuser l’accès si l’âge est < 18
+Refuser l’accès si le nombre de tentatives ≥ 3
+Sinon autoriser l’accès
+Le programme doit continuer à demander les informations
+tant que l’accès est refusé,
+sauf si le nombre de tentatives atteint 3."""
---
+++ Python program/notes_manager.py
@@ -0,0 +1,13 @@
+"""
+🧠 Exercice : Mini système de gestion de notes
+Tu dois écrire un programme qui :
+Demande à l’utilisateur combien d’étudiants il veut entrer.
+Pour chaque étudiant :
+Demande son nom
+Demande sa note (entre 0 et 20)
+Vérifie que la note est valide (sinon redemande)
+À la fin :
+Affiche la moyenne
+Affiche combien ont validé (note ≥ 10)
+Affiche le meilleur étudiant
+Affiche les étudiants en rattrapage (note < 8)"""
