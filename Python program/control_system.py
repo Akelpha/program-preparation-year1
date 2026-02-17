848775e037f8400a5ff7f515dcbcbf354fe3f362
Keren Nzira <kerennzira@gmail.com>
2026-02-17 20:48:41 +0000

    feat: update control system exercise to improve user input handling and access logic

--- Python program/control_system.py
+++ Python program/control_system.py
@@ -3,7 +3,7 @@ Tu dois écrire un programme qui :
 Demande à l’utilisateur :
 son nom
 son âge
-le nombre de tentatives qu’il a déjà faites
+le nombre de tentatives 
 Le programme doit :
 Refuser l’accès si l’âge est < 18
 Refuser l’accès si le nombre de tentatives ≥ 3
@@ -11,3 +11,17 @@ Sinon autoriser l’accès
 Le programme doit continuer à demander les informations
 tant que l’accès est refusé,
 sauf si le nombre de tentatives atteint 3."""
+
+name = input("What is your name ?")
+age = int(input("What is your age?"))
+tentative = 0
+
+while tentative < 3 :
+    name = input("What is your name ?")
+    age = int(input("What is your age?"))
+    if(age < 18):
+        print("Access denied")
+        tentative +=1
+    break
+if(tentative>=3):
+    print("Tentative number reached!!")
