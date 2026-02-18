d53ac1cbe325b06b8513e57f3c2d1773555bf7af
Keren Nzira <kerennzira@gmail.com>
2026-02-18 22:04:05 +0000

    feat: enhance notes manager with input validation and GPA calculation

--- Python program/notes_manager.py
+++ Python program/notes_manager.py
@@ -11,3 +11,37 @@ Affiche la moyenne
 Affiche combien ont validé (note ≥ 10)
 Affiche le meilleur étudiant
 Affiche les étudiants en rattrapage (note < 8)"""
+
+student_number = int(input("Enter the student number: "))
+total = 0
+count_valid = 0
+count_rattrapage =0
+for i in range(0,student_number,1):
+    name=input("Enter the student name.")
+    note = float(input("Enter the student note. "))
+    while note < 0 or note > 20:
+        note = float(input("Invalid note. Enter the student note again: "))
+        if 0 <= note >= 20 : 
+            break
+
+    total +=note
+    if note >= 10:
+        count_valid +=1
+    elif note >=8:
+        count_rattrapage+=1
+    if i == 0:
+        best_note = note
+        best_noteName = name
+    else:
+        if note > best_note:
+            best_note = note
+            best_noteName = name
+
+    
+
+print(f"The GPA general is {total/student_number}")
+print(f"the student who validate are {count_valid} and those who are cathing up are {count_rattrapage}") 
+print(f"The best student is {best_noteName} and scored {best_note}") 
+
+        
+    
--- Python program/prod.py
+++ Python program/prod.py
@@ -1,14 +1,14 @@
-n =int(input("Entez une valeur: "))
-p =n*2
-while p < 20:
-    p=p*2
-    print(p)
+# n =int(input("Entez une valeur: "))
+# p =n*2
+# while p < 20:
+#     p=p*2
+#     print(p)
 
 
 # Utilisation de Break
-# while True:
-#     n=int(input("Entrer une valeur"))
-#     s=n**2
-#     print(s)
-#     if(n==0):
-#         break
+while True:
+    n=int(input("Entrer une valeur"))
+    s=n**2
+    print(s)
+    if(n==0):
+        break
