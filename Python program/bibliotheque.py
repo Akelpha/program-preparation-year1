0bf69287f9c482c3ebe24fbc1b794aa9950f10d0
akelpha <kerennzira@gmail.com>
2026-06-02 13:55:29 +0100

    feat: add afficher_personne method to Personne class; implement afficher_details and __str__ methods in Livre class

--- Python program/bibliotheque.py
+++ Python program/bibliotheque.py
@@ -8,7 +8,10 @@ class Personne:
     def set_age(self,newage):
         self.__age = newage
         return self.__age
-    
+    def afficher_personne(self):
+        print(f"Nom : {self.nom}")
+        print(f"Prénom : {self.prenom}")
+        print(f"Âge : {self.__age}")
 class Membre(Personne):
     def __init__(self,nom,prenom,age,num_member):
         super().__init__(nom,prenom,age)
@@ -25,7 +28,18 @@ class Membre(Personne):
 class Livre:
     def __init__(self,titre,auteur,annee_publication):
         self.auteur = auteur
-class bibliotheque(Personne):
+        self.titre = titre
+        self.annee_publication = annee_publication
+
+    def afficher_details(self):
+       print(f"Titre : {self.titre}")
+       print(f"Auteur : {self.auteur}")
+       print(f"Année de publication : {self.annee_publication}")
+
+    def __str__(self):
+        return f"{self.titre} - {self.auteur} ({self.annee_publication})"
+
+class bibliothecaire(Personne):
     def __init__(self,nom,prenom,age,num_employe,role):
         super().__init__(nom,prenom,age)
         self.__num_employe = num_employe
